716fca574d9891885efe04fbb3d664333be0713b
Pascal Urien <pascal.urien@gmail.com>
2026-05-04 19:13:59 +0200

    Update makeall.sh

--- keystore2/makeall.sh
+++ keystore2/makeall.sh
@@ -5,9 +5,11 @@ cd ..
 cd ./winscard1
 ./make2.sh
 cd ..
-cd ./src_release7
-./makeubuntu.sh
-./makeubuntuf.sh
+cd ./src_release8
+# use PCSC library
+# ./make8.sh
+# no PCSC
+./make8m.sh
 cd ..
 
 
